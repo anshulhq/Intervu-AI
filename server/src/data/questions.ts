f416e64f76632f05dd53a13dd4b5c0d51ecb798a
Anshul Kumar <15anshulkumar@gmail.com>
2026-06-30 01:19:03 +0530

    feat: add Number of Islands question definition

--- server/src/data/questions.ts
+++ server/src/data/questions.ts
@@ -779,6 +779,26 @@ class Solution {
     category: "Arrays & Hashing",
     tags: ["array", "two-pointers", "dynamic-programming", "stack", "monotonic-stack"],
   },
+  {
+    id: "number-of-islands",
+    title: "Number of Islands",
+    description:
+      "Given an `m x n` 2D binary grid `grid` which represents a map of `'1'`s (land) and `'0'`s (water), return the number of islands.\n\nAn island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.",
+    examples: [
+      "Input: grid = [\n  [\"1\",\"1\",\"1\",\"1\",\"0\"],\n  [\"1\",\"1\",\"0\",\"1\",\"0\"],\n  [\"1\",\"1\",\"0\",\"0\",\"0\"],\n  [\"0\",\"0\",\"0\",\"0\",\"0\"]\n]\nOutput: 1",
+      "Input: grid = [\n  [\"1\",\"1\",\"0\",\"0\",\"0\"],\n  [\"1\",\"1\",\"0\",\"0\",\"0\"],\n  [\"0\",\"0\",\"1\",\"0\",\"0\"],\n  [\"0\",\"0\",\"0\",\"1\",\"1\"]\n]\nOutput: 3",
+    ],
+    starterCode: `class Solution {
+    public int numIslands(char[][] grid) {
+        
+    }
+}`,
+    language: "java",
+    fileName: "Solution.java",
+    difficulty: "medium",
+    category: "Graphs",
+    tags: ["depth-first-search", "breadth-first-search", "union-find", "matrix", "graphs"],
+  },
 ];
 
 /**
